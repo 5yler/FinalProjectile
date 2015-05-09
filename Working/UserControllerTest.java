85e8abbbc4943ddd6f9451cf84eec49e488b24ee
wheatca <caitkw@mit.edu>
2015-05-08 21:46:34 -0400

    Added Test Classes

---
+++ Working/UserControllerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+public class UserControllerTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Working/VehicleControllerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+public class VehicleControllerTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
