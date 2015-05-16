2bb259d400f5971cba8ca1e0f4f6d899bb2ca353
Syler <syler@mit.edu>
2015-05-16 18:12:58 -0400

    Added more methods to ControlTest

--- Working/Tests/ControlTest.java
+++ Working/Tests/ControlTest.java
@@ -11,43 +11,59 @@ import org.junit.Test;
 
 public class ControlTest {
 
-	  @Test(expected=IllegalArgumentException.class)
-		public void makeLowS(){
+	@Test(expected=IllegalArgumentException.class)
+	public void makeLowS(){
 		// an invalid low s value should throw an exception
 		new Control(0.5*GroundVehicle.MIN_VEL,.3);
-	  }
-		
-	  @Test(expected=IllegalArgumentException.class)
-	    public void makeHighS(){
-	    // an invalid high s value should throw an exception
-	    new Control(GroundVehicle.MAX_VEL+1,.3);
-	  }
-	  
-	  @Test(expected=IllegalArgumentException.class)
-	    public void makeLowOmega(){
-	      // an invalid low omega value should throw an exception
-		  double omega = -Math.PI - 1;
-		  new Control(5,omega);
-	  }
-	  
-	  @Test(expected=IllegalArgumentException.class)
-	    public void makeHighOmega(){
-	      // an invalid high omega value should throw an exception
-		  double omega = Math.PI + 1;
-		  new Control(5,omega);
-	  }
-	  
-	  @Test
-	    public void CheckOutputs(){
-	    double s = 7.5d;
-	    double omega = .3d;
-	    Control test = new Control(s, omega);
-	    assertEquals(omega, test.getRotVel(), 1e-6);
-	    assertEquals(s, test.getSpeed(), 1e-6);
-	  }
-	  
-	  public static void main(String[] args){
-	    JUnitCore.main(ControlTest.class.getName());
-	  }
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void makeHighS(){
+		// an invalid high s value should throw an exception
+		new Control(GroundVehicle.MAX_VEL+1,.3);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void makeLowOmega(){
+		// an invalid low omega value should throw an exception
+		double omega = -Math.PI - 1;
+		new Control(5,omega);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void makeHighOmega(){
+		// an invalid high omega value should throw an exception
+		double omega = Math.PI + 1;
+		new Control(5,omega);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void makeHighBoundaryOmega(){
+		// setting omega to higher, non-inclusive limit of interval
+		// should throw an exception
+		double omega = Math.PI;
+		Control c = new Control(5,omega);
+	}
+
+	@Test
+	public void CheckOutputs(){
+		double s = 7.5d;
+		double omega = .3d;
+		Control c = new Control(s, omega);
+		assertEquals(omega, c.getRotVel(), 1e-6);
+		assertEquals(s, c.getSpeed(), 1e-6);
+	}
+
+	@Test
+	public void makeLowBoundaryOmega(){
+		// setting omega to lower, inclusive limit of interval
+		double omega = -Math.PI;
+		Control c = new Control(5,omega);
+		assertEquals(-Math.PI,c.getRotVel(),1e-6);
+	}
+
+	public static void main(String[] args){
+		JUnitCore.main(ControlTest.class.getName());
+	}
 
 }
