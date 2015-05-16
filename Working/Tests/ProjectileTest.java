9bf66fe2cc37df94b962e3291113e664f6a1dd15
Syler <syler@mit.edu>
2015-05-16 19:06:14 -0400

    Added tests for Projectile shoot() method

--- Working/Tests/ProjectileTest.java
+++ Working/Tests/ProjectileTest.java
@@ -18,12 +18,10 @@ public class ProjectileTest {
 		double [] pose = {1, 2, 0};
 		double dx = 5, dy = 0, dtheta = 0;
 		double [] shooterPos = pose;
-		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dt);
+		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dtheta);
 
-		DisplayServer ds = new DisplayServer();
-		DisplayClient dc = new DisplayClient();
 		Simulator sim = new Simulator();
-		UserController uc = new UserController(sim, v, ds);
+		UserController uc = new UserController(sim, gv);
 
 		Projectile p = new Projectile(shooterPos, sim, uc);
 
@@ -42,12 +40,10 @@ public class ProjectileTest {
 		double [] pose = {1, 2, 0};
 		double dx = 5, dy = 0, dtheta = 0;
 		double [] shooterPos = {3, 4, 5, 6};
-		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dt);
+		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dtheta);
 
-		DisplayServer ds = new DisplayServer();
-		DisplayClient dc = new DisplayClient();
 		Simulator sim = new Simulator();
-		UserController uc = new UserController(sim, v, ds);
+		UserController uc = new UserController(sim, gv);
 
 		Projectile p = new Projectile(shooterPos, sim, uc);
 	}
@@ -58,12 +54,10 @@ public class ProjectileTest {
 		double [] pose = {1, 2, 0};
 		double dx = 5, dy = 0, dtheta = 0;
 		double [] shooterPos = {3};
-		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dt);
+		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dtheta);
 
-		DisplayServer ds = new DisplayServer();
-		DisplayClient dc = new DisplayClient();
 		Simulator sim = new Simulator();
-		UserController uc = new UserController(sim, v, ds);
+		UserController uc = new UserController(sim, gv);
 
 		Projectile p = new Projectile(shooterPos, sim, uc);
 	}
@@ -71,28 +65,88 @@ public class ProjectileTest {
 
 	/**
 	 * Method: shoot(int sec, int msec)
+	 *
+	 * Tests if projectile moves appropriate amount along x when shot
 	 */
-	public void testShoot(){
-		double [] pose = {1, 1, 0};
+	@Test
+	public void testShootX(){
+
+		// create vehicle facing along positive x direction
+		double [] shooterPosition = {1, 1, 0};
 		double dx = 5, dy = 0, dtheta = 0;
-		double [] shooterPos = pose;
-		GroundVehicle gv = new GroundVehicle(pose, dx, dy, dt);
+		GroundVehicle gv = new GroundVehicle(shooterPosition, dx, dy, dtheta);
 
-		DisplayServer ds = new DisplayServer();
-		DisplayClient dc = new DisplayClient();
 		Simulator sim = new Simulator();
-		UserController uc = new UserController(sim, v, ds);
+		UserController uc = new UserController(sim, gv);
 
-		Projectile p = new Projectile(shooterPos, sim, uc);
-		
-		p.shoot(1,0);
+		Projectile p = new Projectile(shooterPosition, sim, uc);
+
+		// check if projectile moves appropriate amount after one second
+		p.shoot(1, 0);
 		
 		double [] newPose = p.getPosition();
-        assertEquals(121, newPose[0], 1e-6);
-        assertEquals(0, newPose[1], 1e-6);
-        assertEquals(0, newPose[2], 1e-6);
-		
-		
+        assertEquals(shooterPosition[0]+Projectile.PROJECTILE_SPEED, newPose[0], 1e-6);
+        assertEquals(shooterPosition[1], newPose[1], 1e-6);
+        assertEquals(shooterPosition[2], newPose[2], 1e-6);
+	}
+
+	/**
+	 * Method: shoot(int sec, int msec)
+	 *
+	 * Tests if projectile moves appropriate amount along y when shot
+	 */
+	@Test
+	public void testShootY(){
+
+		// create vehicle facing along positive y direction
+		double [] shooterPosition = {1, 1, Math.PI/2};
+		double dx = 5, dy = 0, dtheta = 0;
+		GroundVehicle gv = new GroundVehicle(shooterPosition, dx, dy, dtheta);
+
+		Simulator sim = new Simulator();
+		UserController uc = new UserController(sim, gv);
+
+		Projectile p = new Projectile(shooterPosition, sim, uc);
+
+		// check if projectile moves appropriate amount after one second
+		p.shoot(1, 0);
+
+		double [] newPose = p.getPosition();
+		assertEquals(shooterPosition[0], newPose[0], 1e-6);
+		assertEquals(shooterPosition[1]+Projectile.PROJECTILE_SPEED, newPose[1], 1e-6);
+		assertEquals(shooterPosition[2], newPose[2], 1e-6);
+	}
+
+	/**
+	 * Method: shoot(int sec, int msec)
+	 *
+	 * Tests if projectile moves appropriate amount along
+	 * both x and y when shot at an angle
+	 */
+	@Test
+	public void testShootXandY(){
+
+		// create vehicle facing along positive y direction
+		double shooterAngle = Math.PI/4;
+		double [] shooterPosition = {1, 1, shooterAngle};
+		double dx = 5, dy = 0, dtheta = 0;
+		GroundVehicle gv = new GroundVehicle(shooterPosition, dx, dy, dtheta);
+
+		Simulator sim = new Simulator();
+		UserController uc = new UserController(sim, gv);
+
+		Projectile p = new Projectile(shooterPosition, sim, uc);
+
+		// check if projectile moves appropriate amount after one second
+		p.shoot(1, 0);
+
+		double expectedX = shooterPosition[0]+Math.cos(shooterAngle)*Projectile.PROJECTILE_SPEED;
+		double expectedY = shooterPosition[1]+Math.sin(shooterAngle)*Projectile.PROJECTILE_SPEED;
+
+		double [] newPose = p.getPosition();
+		assertEquals(expectedX, newPose[0], 1e-6);
+		assertEquals(expectedY, newPose[1], 1e-6);
+		assertEquals(shooterAngle, newPose[2], 1e-6);
 	}
 
 
