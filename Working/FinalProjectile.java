2767d46ee751515fa7ac64e6841bb63160d367f1
Syler <syler@mit.edu>
2015-05-15 22:15:44 -0400

    Cleaned up FinalProjectile

--- Working/FinalProjectile.java
+++ Working/FinalProjectile.java
@@ -11,35 +11,24 @@ import java.net.Socket;
 
 public class FinalProjectile {
 
-    private static final boolean lead3  = true;    // set to true for multiple LeadingController test
-    public static boolean MULTIPLAYER;    // multiplaye mode
+    public static boolean MULTIPLAYER;      // multiplaye mode
+    public static int NUM_VEHICLES;         // number of non-user vehicles
+
+    public static final int GAME_TIME = 200;          // [s] time the game runs for //TODO: req
+    public static final int GAME_OVER_TIMEOUT = 10;   // [s] time the display remains after game over  //TODO: req
+
+    // [ms] time between successive updates for various threads
+    public static final int SIMULATOR_MS = 50;      //TODO: req
+    public static final int VEHICLE_MS = 50;        //TODO: req
+    public static final int CONTROLLER_MS = 100;    //TODO: req
+    public static final int PROJECTILE_MS = 20;     //TODO: req
 
     // debug print statement toggle
-    public static final boolean debug_follower      = true;    // set to true for FC debug
-    public static final boolean debug_user          = true;    // set to true for FC debug
+    public static final boolean debug_user          = false;    // set to true for user controller debug
     public static final boolean debug_projectiles   = false;    // set to true for projectile debug
     public static final boolean debug_keys          = false;    // set to true for key event debug
     public static final boolean debug_scores        = false;    // set to true for score display message debug
     public static final boolean debug_display_msgs  = false;    // set to true for projectile/vehicle display msg debug
-
-
-    // [ms] time between successive updates for various threads
-    public static final int SIMULATOR_MS = 50;  //TODO: req
-    public static final int VEHICLE_MS = 50;  //TODO: req
-    public static final int CONTROLLER_MS = 100;  //TODO: req
-    public static final int PROJECTILE_MS = 20;  //TODO: req
-
-    // colors
-    public static final int USER1_COLOR = 1;
-    public static final int USER2_COLOR = 2;
-    public static final int LEADING_COLOR = 3;
-    public static final int FOLLOWING_COLOR = 4;
-
-    public static final int GAME_TIME = 200;        // [s] time the game runs for //TODO: req
-    public static final int GAME_OVER_TIMEOUT = 10;   // [s] time the display remains after game over  //TODO: req
-    //TODO: make it stop when all non-user vehicles disappear
-
-    public static int NUM_VEHICLES;
 
     /* MAIN METHOD */
     public static void main(String[] args) {
@@ -86,21 +75,8 @@ public class FinalProjectile {
             System.out.println("Single player mode enabled.");
         }
 
-//            System.out.println("User 1 controls: move with [^]UP [v]DOWN [<]LEFT [>]RIGHT keys, shoot with [.]SPACE");
-        if (MULTIPLAYER) {
-//            System.out.println("User 2 controls: move with [^]W  [v]S    [<]A    [>]D     keys, shoot with [.]SHIFT");
-        }
-
-        // sleep for two seconds so user can read controls
-        try {
-            Thread.sleep(5*100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
         double[] pos1 = {10, 10, 0};
-        double[] pos2 = {Simulator.SIM_X-10, Simulator.SIM_Y-10, Math.PI};
+        double[] pos2 = {Simulator.SIM_X - 10, Simulator.SIM_Y - 10, Math.PI};
 
         try {
             ServerSocket s = new ServerSocket(5065);
@@ -124,23 +100,13 @@ public class FinalProjectile {
             sim.addUserController(uc1);
 
             // create non-user vehicles
-            for (int i = 1; i < NUM_VEHICLES; i++) {
+            for (int i = 0; i < NUM_VEHICLES; i++) {
                 GroundVehicle gv = new GroundVehicle(sim.randomStartingPosition(), sim.randomDoubleInRange(0, 10), sim.randomDoubleInRange(-Math.PI / 4, Math.PI / 4));
                 VehicleController vc = new LeadingController(sim, gv);
                 sim.addVehicle(gv);
                 gv.start();
                 vc.start();
             }
-
-            if(debug_follower) {
-                GroundVehicle fv = new GroundVehicle(sim.randomStartingPosition(), sim.randomDoubleInRange(0, 10), sim.randomDoubleInRange(-Math.PI / 4, Math.PI / 4));
-                FollowingController fc = new FollowingController(sim, fv, uv1);
-                sim.addFollowingController(fc);
-                sim.addVehicle(fv);
-                fv.start();
-                fc.start();
-            }
-
 
             uv1.start();
             uc1.start();
@@ -168,42 +134,6 @@ public class FinalProjectile {
             System.err.println(e);
             System.exit(-1);
         }
-
-//            DisplayServer ds = new DisplayServer(host);
-        // create DisplayClient
-/*
-            double[] pos = {10, 10, 0};
-
-            // construct a single GroundVehicle
-//            GroundVehicle gv = new GroundVehicle(
-//                    Simulator.randomStartingPosition(),
-//                    Simulator.randomDoubleInRange(0, 10),
-//                    Simulator.randomDoubleInRange(-Math.PI / 4, Math.PI / 4));
-
-            GroundVehicle gv = new GroundVehicle(pos, 1, 0);
-
-
-            Thread gvThread = new Thread(gv);
-
-            // construct a single Simulator
-            Simulator sim = new Simulator(dc);
-            sim.addVehicle(gv);
-            Thread simThread = new Thread(sim);
-
-            // construct a single instance of the CircleController class
-            UserController uc = new UserController(sim,gv);
-            Thread ucThread = new Thread(uc);
-
-            gv.start();
-            uc.start();
-            sim.start();
-
-        } else { // if wrong number of arguments
-            System.err.println("Allowed format:");
-            System.err.println("$ java FinalProjectile <IP>");
-            System.exit(-1);
-        }
-    }*/
     }
 }
 
